98f87801aad8581659d05d69cf01ff4c8e3184bc
bigsurmoon <vinny4000@gmail.com>
2021-09-28 05:53:49 +0700

    init commit

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,14 @@
+public class CreditPaymentService {
+    public int calculate(int creditSummary, int years) {
+
+        double rate = 9.99;
+        double monthlyPercentRate = rate / 12 / 100;
+        int creditTerm = years * 12;
+        double annuityRate = (monthlyPercentRate * Math.pow((1 + monthlyPercentRate), creditTerm)) / (Math.pow((1 + monthlyPercentRate), creditTerm) - 1);
+        double monthlyAnnuityPayment = annuityRate * creditSummary;
+        double totalMonthlyPayment = (int) monthlyAnnuityPayment;
+
+        return (int) totalMonthlyPayment;
+
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        CreditPaymentService service = new CreditPaymentService();
+
+
+        int oneYearMonthlyPayment = service.calculate(1_000_000, 1);
+        System.out.println("Ежемесячная процентная ставка за 1 год: " + oneYearMonthlyPayment);
+
+        int twoYearsMonthlyPayment = service.calculate(1_000_000, 2);
+        System.out.println("Ежемесячная процентная ставка за 2 года: " + twoYearsMonthlyPayment);
+
+        int threeYearMonthlyPayment = service.calculate(1_000_000, 3);
+        System.out.println("Ежемесячная процентная ставка за 3 года: " + threeYearMonthlyPayment);
+
+
+    }
+
+}
